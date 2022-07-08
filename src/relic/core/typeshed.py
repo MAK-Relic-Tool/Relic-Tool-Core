87a6da8ae176b257b8ee34f0cf0fb80c1ae27082
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-07-08 15:59:23 -0800

    Pylint fixes

--- src/relic/core/typeshed.py
+++ src/relic/core/typeshed.py
@@ -1,10 +1,12 @@
+"""
+Provides a version safe interface for retrieving attributes from the typing / typing_extensions modules.
+"""
 import sys
 
 __version_pair = sys.version_info[0:2]
 if __version_pair >= (3, 10):
-    from typing import TypeAlias
+    from typing import TypeAlias  # pylint: disable=no-name-in-module
 else:
-    from typing_extensions import TypeAlias
-
+    from typing_extensions import TypeAlias  # pylint: disable=no-name-in-module
 
 __all__ = [TypeAlias]
