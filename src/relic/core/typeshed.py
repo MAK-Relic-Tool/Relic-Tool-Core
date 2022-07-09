c36db75dbc783bb8f58219c3f87617fc98546354
Marcus Kertesz <ModernMAK@Hotmail.com>
2022-07-08 16:36:41 -0800

    Update typeshed.py
    Fix mypy/pylint errors
    
    __version_pair will break mypy's version checking

--- src/relic/core/typeshed.py
+++ src/relic/core/typeshed.py
@@ -3,10 +3,11 @@ Provides a version safe interface for retrieving attributes from the typing / ty
 """
 import sys
 
-__version_pair = sys.version_info[0:2]
-if __version_pair >= (3, 10):
+if sys.version_info[0:2] >= (3, 10):
     from typing import TypeAlias  # pylint: disable=no-name-in-module
 else:
     from typing_extensions import TypeAlias  # pylint: disable=no-name-in-module
 
-__all__ = [TypeAlias]
+__all__ = [
+    "TypeAlias",
+]
