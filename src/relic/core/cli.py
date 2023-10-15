cdc093eeca674101cbe5c87efc8f0148cd692317
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-14 18:06:42 -0700

    MyPy | Black | Pylint fixes

--- src/relic/core/cli.py
+++ src/relic/core/cli.py
@@ -2,30 +2,42 @@ from __future__ import annotations
 
 import sys
 from argparse import ArgumentParser, Namespace
-from typing import Optional, TYPE_CHECKING, Protocol
+from typing import Optional, TYPE_CHECKING, Protocol, Any
 
 import pkg_resources
 
 if TYPE_CHECKING:
-    class _SubParsersAction:
-        def add_parser(self, name, *, prog: Optional = None, aliases: Optional = None, help: Optional = None,
-                       **kwargs) -> ArgumentParser:
+    # Circumvent mypy/pylint shenanigans
+    class _SubParsersAction:  # pylint: disable= too-few-public-methods # typechecker only, ignore warnings
+        def add_parser(  # pylint: disable=redefined-builtin, unused-argument # typechecker only, ignore warnings
+            self,
+            name: str,
+            *,
+            prog: Optional[str] = None,
+            aliases: Optional[Any] = None,
+            help: Optional[str] = None,
+            **kwargs: Any,
+        ) -> ArgumentParser:
             ...
 
 
-class CliEntrypoint(Protocol):
-    def __call__(self, parent: _SubParsersAction, autoload: bool):
+class CliEntrypoint(Protocol):  # pylint: disable= too-few-public-methods
+    def __call__(
+        self, parent: Optional[_SubParsersAction], autoload: bool
+    ) -> Optional[int]:
         raise NotImplementedError
 
 
-class _CliPlugin:
+class _CliPlugin:  # pylint: disable= too-few-public-methods
     def __init__(self, parser: ArgumentParser):
         self.parser = parser
 
-    def run(self, *args):
+    def run(self, *args: Any) -> None:
         ns = self.parser.parse_args(args)
         if not hasattr(ns, "cmd"):
-            raise NotImplementedError("Command defined in argparse, but it's function was not specified.")
+            raise NotImplementedError(
+                "Command defined in argparse, but it's function was not specified."
+            )
         cmd = ns.cmd
         result = cmd(ns)
         if result is None:  # Assume success
@@ -33,10 +45,10 @@ class _CliPlugin:
         sys.exit(result)
 
 
-class CliPluginGroup(_CliPlugin):
-    GROUP = None
+class CliPluginGroup(_CliPlugin):  # pylint: disable= too-few-public-methods
+    GROUP: str = None  # type: ignore
 
-    def __init__(self, parent: _SubParsersAction, autoload: bool = True, **kwargs):
+    def __init__(self, parent: _SubParsersAction, autoload: bool = True, **kwargs: Any):
         if TYPE_CHECKING:
             self.subparsers = None
         if self.GROUP is None:
@@ -47,37 +59,42 @@ class CliPluginGroup(_CliPlugin):
         if autoload:
             self._load(autoload)
 
-    def _create_parser(self, command_group: Optional[_SubParsersAction] = None) -> ArgumentParser:
+    def _create_parser(
+        self, command_group: Optional[_SubParsersAction] = None
+    ) -> ArgumentParser:
         raise NotImplementedError
 
     def _create_subparser_group(self, parser: ArgumentParser) -> _SubParsersAction:
-        return parser.add_subparsers()
+        return parser.add_subparsers()  # type: ignore
 
-    def _load(self, autoload: bool = False):
+    def _load(self, autoload: bool = False) -> None:
         for ep in pkg_resources.iter_entry_points(group=self.GROUP):
             ep_func: CliEntrypoint = ep.load()
             ep_func(parent=self.subparsers, autoload=autoload)
 
 
-class CliPlugin(_CliPlugin):
-    def __init__(self, parent: Optional[_SubParsersAction], **kwargs):
+class CliPlugin(_CliPlugin):  # pylint: disable= too-few-public-methods
+    def __init__(self, parent: Optional[_SubParsersAction], **kwargs: Any):
         parser = self._create_parser(parent)
         super().__init__(parser)
         if self.parser.get_default("cmd") is None:
             self.parser.set_defaults(cmd=self.command)
 
-    def _create_parser(self, command_group: Optional[_SubParsersAction] = None) -> ArgumentParser:
+    def _create_parser(
+        self, command_group: Optional[_SubParsersAction] = None
+    ) -> ArgumentParser:
         raise NotImplementedError
 
     def command(self, ns: Namespace) -> Optional[int]:
         raise NotImplementedError
 
 
-class RelicCli(CliPluginGroup):
+class RelicCli(CliPluginGroup):  # pylint: disable= too-few-public-methods
     GROUP = "relic.cli"
 
-    def _create_parser(self, command_group: Optional[_SubParsersAction] = None) -> ArgumentParser:
+    def _create_parser(
+        self, command_group: Optional[_SubParsersAction] = None
+    ) -> ArgumentParser:
         if command_group is None:
             return ArgumentParser("relic")
-        else:
-            return command_group.add_parser("relic")
+        return command_group.add_parser("relic")
