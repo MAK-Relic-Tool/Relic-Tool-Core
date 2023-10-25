aaaffba1f6563773a39c35e0c4fd1ea0045ae2e9
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-25 03:56:22 -0700

    Allow run with to ignore 'program name' when passed

--- src/relic/core/cli.py
+++ src/relic/core/cli.py
@@ -43,7 +43,7 @@ class _CliPlugin:  # pylint: disable= too-few-public-methods
 
     def run_with(self, *args: Any) -> Union[str, int, None]:
         if len(args) > 0 and self.parser.prog == args[0]:
-            args = args[1:] # allow prog to be first command
+            args = args[1:]  # allow prog to be first command
         try:
             ns = self.parser.parse_args(args)
             return self._run(ns)
