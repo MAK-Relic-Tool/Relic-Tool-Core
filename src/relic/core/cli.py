a3ff7f757e8d2a208610fc9a8e168bd281147ba1
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-29 01:40:58 -0700

    Fix CI integration failures when setting up Staging branch (#8)
    * Buffer not defined because typing-extension not installed
    
    * Fix name no longer supporting None
    
    * Resolved TODOs
    
    * Update .pylintrc to properly calculate score, also lower pylint standards
    
    * Docstrings + Formatting
    
    * More docstrings + raising proper Errors
    
    * More docstrings and lowering pylint standards
    
    * Apply Black

--- src/relic/core/cli.py
+++ src/relic/core/cli.py
@@ -1,3 +1,8 @@
+"""
+Core files for implementing a Command Line Interface using Entrypoints
+
+
+"""
 from __future__ import annotations
 
 import sys
@@ -6,9 +11,15 @@ from typing import Optional, TYPE_CHECKING, Protocol, Any, Union
 
 import pkg_resources
 
+from relic.core.errors import UnboundCommandError
+
 
 # Circumvent mypy/pylint shenanigans ~
 class _SubParsersAction:  # pylint: disable= too-few-public-methods # typechecker only, ignore warnings
+    """
+    A Faux class to fool MyPy because argparser does python magic to bind subparsers to their parent parsers
+    """
+
     def add_parser(  # pylint: disable=redefined-builtin, unused-argument # typechecker only, ignore warnings
         self,
         name: str,
@@ -18,11 +29,27 @@ class _SubParsersAction:  # pylint: disable= too-few-public-methods # typechecke
         help: Optional[str] = None,
         **kwargs: Any,
     ) -> ArgumentParser:
+        """
+        Adds a parser to the parent parser this is binded to.
+        See argparse for more details.
+        """
         raise NotImplementedError
 
 
 class CliEntrypoint(Protocol):  # pylint: disable= too-few-public-methods
-    def __call__(self, parent: Optional[_SubParsersAction]) -> Optional[int]:
+    """
+    A protocol defining the expected entrypoint format when defining CLI Plugins
+    """
+
+    def __call__(self, parent: Optional[_SubParsersAction]) -> None:
+        """
+        Attach a parser to the parent subparser group.
+        :param parent: The parent subparser group, if None, this is not being loaded as an entrypoint
+        :type parent: Optional[_SubParsersAction]
+
+        :returns: Nothing, if something is returned it should be ignored
+        :rtype: None
+        """
         raise NotImplementedError
 
 
@@ -31,17 +58,40 @@ class _CliPlugin:  # pylint: disable= too-few-public-methods
         self.parser = parser
 
     def _run(self, ns: Namespace) -> int:
-        if not hasattr(ns, "cmd"):
-            raise NotImplementedError(
-                "Command defined in argparse, but it's function was not specified."
-            )
-        cmd = ns.cmd
-        result: Optional[int] = cmd(ns)
+        """
+        Run the command using args provided by namespace
+
+        :param ns: The namespace containing the args the command was called with
+        :type ns: Namespace
+
+        :raises NotImplementedError: The command was defined, but was not bound to a function
+
+        :returns: An integer representing the status code; 0 by default if the command does not return a status code
+        :rtype: int
+        """
+
+        if hasattr(ns, "command"):
+            cmd = ns.command
+        else:
+            cmd = self.parser.prog
+
+        if not hasattr(ns, "function"):
+            raise UnboundCommandError(cmd)
+        func = ns.function
+        result: Optional[int] = func(ns)
         if result is None:  # Assume success
             result = 0
         return result
 
-    def run_with(self, *args: Any) -> Union[str, int, None]:
+    def run_with(self, *args: str) -> Union[str, int, None]:
+        """
+        Run the command line interface with the given arguments.
+        :param args: The arguments that will be run on the command line interface.
+        :type args: str
+
+        :returns: The status code or status message.
+        :rtype: Union[str,int,None]
+        """
         if len(args) > 0 and self.parser.prog == args[0]:
             args = args[1:]  # allow prog to be first command
         try:
@@ -51,6 +101,12 @@ class _CliPlugin:  # pylint: disable= too-few-public-methods
             return sys_exit.code
 
     def run(self) -> None:
+        """
+        Run the command line interface, using arguments from sys.argv, then terminates the process.
+
+        :returns: Nothing; the process is terminated
+        :rtype: None
+        """
         ns = self.parser.parse_args()
         exit_code = self._run(ns)
         sys.exit(exit_code)
@@ -78,7 +134,7 @@ class CliPluginGroup(_CliPlugin):  # pylint: disable= too-few-public-methods
         raise NotImplementedError
 
     def _create_subparser_group(self, parser: ArgumentParser) -> _SubParsersAction:
-        return parser.add_subparsers()  # type: ignore
+        return parser.add_subparsers(dest="command")  # type: ignore
 
     def _load(self) -> None:
         for ep in pkg_resources.iter_entry_points(group=self.GROUP):
@@ -90,8 +146,8 @@ class CliPlugin(_CliPlugin):  # pylint: disable= too-few-public-methods
     def __init__(self, parent: Optional[_SubParsersAction] = None):
         parser = self._create_parser(parent)
         super().__init__(parser)
-        if self.parser.get_default("cmd") is None:
-            self.parser.set_defaults(cmd=self.command)
+        if self.parser.get_default("function") is None:
+            self.parser.set_defaults(function=self.command)
 
     def _create_parser(
         self, command_group: Optional[_SubParsersAction] = None
@@ -114,3 +170,6 @@ class RelicCli(CliPluginGroup):  # pylint: disable= too-few-public-methods
 
 
 cli_root = RelicCli()
+
+if __name__ == "__main__":
+    cli_root.run()
--- src/relic/core/errors.py
+++ src/relic/core/errors.py
@@ -5,6 +5,27 @@ from typing import Any, Optional, TypeVar, Generic
 
 
 def _print_mismatch(name: str, received: Optional[Any], expected: Optional[Any]) -> str:
+    """
+    Constructs a string detailing a mismatch between a received and expected input
+
+    :param name: The name of the variable which received unexpected input
+    :type name: str
+
+    :param received: The value of the received input
+    :type received: Optional[Any]
+
+    :param expected: The value(s) of the expected input
+    :type expected: Optional[Any]
+
+    :return: A string formatted as one of the following (varies by input)
+        'Unexpcted {name}!'
+        'Unexpcted {name}; got {recieved}!'
+        'Unexpcted {name}; expected {expected}!'
+        'Unexpcted {name}; got {recieved}, expected {expected}!'
+
+    :rtype: str
+    """
+
     msg = f"Unexpected {name}"
     if received is not None or expected is not None:
         msg += ";"
@@ -22,8 +43,32 @@ T = TypeVar("T")
 
 class RelicToolError(Exception):
     """
-    Marks an Error as a RelicToolError. Does nothing special.
+    Marks an Error as a RelicToolError.
+    Does nothing special.
+    All non-standard errors should inherit from this class.
     """
+
+
+class CliError(RelicToolError):
+    """
+    Marks an Error as a Command Line Error.
+    Does nothing special.
+    All command line errors should inherit from this class.
+    """
+
+
+class UnboundCommandError(CliError):
+    """
+    A command was defined in the CLI, but its function was not bound.
+
+    If a command is meant to do nothing, a 'do-nothing' function should be bound instead.
+    """
+
+    def __init__(self, name: str):
+        self._name = name
+
+    def __str__(self) -> str:
+        return f"The '{self._name}' command was defined, but not bound to a function."
 
 
 class MismatchError(Generic[T], RelicToolError):
