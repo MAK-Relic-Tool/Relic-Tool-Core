752730febc9ab2058bff4d79dbb26e58087b9222
Marcus Kertesz <ModernMAK@Hotmail.com>
2023-10-29 01:31:36 -0700

    More docstrings and lowering pylint standards

--- src/relic/core/lazyio.py
+++ src/relic/core/lazyio.py
@@ -13,7 +13,6 @@ from typing import (
     Tuple,
     Dict,
     Optional,
-    TypeVar,
     Any,
     Literal,
     Protocol,
@@ -23,21 +22,47 @@ from typing import (
     List,
 )
 
-from relic.core.typeshed import Buffer
 from relic.core.errors import RelicToolError, MismatchError
+from relic.core.typeshed import Buffer
 
+ByteOrder = Literal["big", "little"]
+
+
+_KIBIBYTE = 1024
 
 @runtime_checkable
 class BinaryProxy(Protocol):  # pylint: disable=too-few-public-methods
+    """
+    A Protocol allowing classes to proxy being a BianryIO to lazyio classes
+    """
     def __binio_proxy__(self) -> Union[BinaryIO, BinaryProxy]:
+        """
+        Get the instance this class proxies to
+
+        :returns: The BinaryIO or BinaryProxy that this object proxies to
+        :rtype: Union[BinaryIO, BinaryProxy]
+        """
         raise NotImplementedError
 
 
 def is_proxy(s: Any) -> bool:
+    """
+    Return whether an object is a Binary Proxy
+
+    :rtype: bool
+    :returns: True if the object is a BinaryProxy, False otherwise
+    """
     return isinstance(s, BinaryProxy)
 
 
 def get_proxy(s: Union[BinaryProxy, BinaryIO]) -> BinaryIO:
+    """
+    Resolves a proxy chain recursively
+
+    :rtype: BinaryIO
+    :returns: The final BinaryIO instance a BinaryProxy proxies to.
+        If the instance is a BianryIO, the instance is returned as is
+    """
     if isinstance(s, BinaryProxy):
         proxy = s.__binio_proxy__()
         return get_proxy(proxy)  # resolve nested proxies
@@ -45,6 +70,10 @@ def get_proxy(s: Union[BinaryProxy, BinaryIO]) -> BinaryIO:
 
 
 class BinaryWrapper(BinaryIO):
+    """
+    Allows a BinaryIO object to be wrapped and subclassed without altering the parent BinaryIO object
+    """
+
     def __init__(
         self,
         parent: Union[BinaryProxy, BinaryIO],
@@ -153,6 +182,11 @@ class BinaryWrapper(BinaryIO):
 
 
 class BinaryWindow(BinaryWrapper):
+    """
+    A BinaryIO which only exposes a 'slice' of the stream
+
+    Maintains an internal pointer to the current position of the window, ignoring the parent stream's current position
+    """
     def __init__(
         self,
         parent: Union[BinaryIO, BinaryProxy],
@@ -237,6 +271,9 @@ class BinaryWindow(BinaryWrapper):
 
 
 class _CStringOps:
+    """
+    Provides utility functions for serializing C-String Buffers
+    """
     def __init__(self, serialzer: BinarySerializer):
         self._serializer = serialzer
 
@@ -291,13 +328,16 @@ class _CStringOps:
                     )
                 buffer = b"".join([buffer, pad_buffer * int(pad_count)])
             elif len(buffer) != size:
-                raise RelicToolError(
-                    f"Trying to write '{size}' bytes, received '{buffer!r}' ({len(buffer)})!"
-                )
+                raise MismatchError("Writing Bytes",len(buffer),size)
         return buffer
 
 
 class _IntOps:
+    """
+    Provides utility functions for serializing Any Integer
+
+    Provides more dynamic read/writes at the expense of less validity checks than _SizedIntOps
+    """
     def __init__(self, serialzer: BinarySerializer):
         self._serializer = serialzer
 
@@ -357,6 +397,11 @@ class _IntOps:
 
 
 class _SizedIntOps(_IntOps):
+    """
+    Provides utility functions for serializing Sized & Signed Integers
+
+    Provides more validation over input/output then _IntOps
+    """
     def __init__(self, serializer: BinarySerializer, size: int, signed: bool):
         super().__init__(serializer)
         self._size = size
@@ -432,6 +477,11 @@ class _SizedIntOps(_IntOps):
 
 
 class BinarySerializer(BinaryProxy):  # pylint: disable= too-many-instance-attributes
+    """
+    A utility object that allows serializing/deserializing most data types
+
+    Acts as a BinaryProxy which points to the parent object it reads from/writes to
+    """
     def __init__(
         self,
         parent: Union[BinaryIO, BinaryProxy],
@@ -488,6 +538,10 @@ class BinarySerializer(BinaryProxy):  # pylint: disable= too-many-instance-attri
 
 
 class BinaryProxySerializer(BinaryProxy):  # pylint: disable= R0903
+    """
+    A Mixin-like class which allows the class to be treated as a BinaryIO via proxying, and automatically creates a serializer to be used to read/write data lazily
+    """
+
     def __init__(
         self,
         stream: Union[BinaryIO, BinaryProxy],
@@ -498,17 +552,11 @@ class BinaryProxySerializer(BinaryProxy):  # pylint: disable= R0903
         return self._serializer
 
 
-ByteOrder = Literal["big", "little"]
-
-
-T = TypeVar("T")
-
-_KILOBYTE = 1024
 
 
 class ZLibFileReader(BinaryWrapper):
     def __init__(
-        self, parent: Union[BinaryIO, BinaryProxy], *, chunk_size: int = 16 * _KILOBYTE
+        self, parent: Union[BinaryIO, BinaryProxy], *, chunk_size: int = 16 * _KIBIBYTE
     ):
         super().__init__(parent)
         self._data_cache: Optional[bytes] = None
@@ -574,6 +622,13 @@ class ZLibFileReader(BinaryWrapper):
 
 
 def tell_end(stream: BinaryIO) -> int:
+    """
+    Gets the index of the end of the stream; unless written to further, this will be the size of the stream
+    :param stream: The stream to get the end of
+
+    :returns: The index of the last position in the stream
+    :rtype: int
+    """
     now = stream.tell()
     end = stream.seek(0, os.SEEK_END)
     stream.seek(now)
@@ -584,8 +639,32 @@ def read_chunks(
     stream: Union[BinaryIO, bytes, bytearray],
     start: Optional[int] = None,
     size: Optional[int] = None,
-    chunk_size: int = _KILOBYTE * 16,
+    chunk_size: int = _KIBIBYTE * 16,
 ) -> Iterable[bytes]:
+    """
+    Yields chunks from the stream until the size or the end of the stream is reached
+
+    :param stream: The bytes-like to read from
+    :type stream: Union[BinaryIO, bytes, bytearray]
+
+    :param start: The offset to start reading from,
+        if None the current position is used (BinaryIO) or the start of the buffer (bytes / bytearray).
+        By default, this is None
+    :type start: Optional[int], optional
+
+    :param size: The maximum number of bytes to read,
+        if None, all bytes will be read.
+        By default, this is None
+    :type size: Optional[int], optional
+
+    :param chunk_size: The maximum number of bytes to yield at a time.
+        By default, this is 16 KiB
+    :type chunk_size: int
+
+    :returns: An iterable of bytes containing all data from start to start + size
+    :rtype: Iterable[bytes]
+    """
+
     if isinstance(stream, (bytes, bytearray)):
         if start is None:
             start = 0
@@ -619,8 +698,34 @@ def chunk_copy(
     src_start: Optional[int] = None,
     size: Optional[int] = None,
     dst_start: Optional[int] = None,
-    chunk_size: int = _KILOBYTE * 16,
+    chunk_size: int = _KIBIBYTE * 16,
 ) -> None:
+    """
+    Copies from a source bytes-like to a destination bytes-like in chunks.
+
+    :param src: The source bytes-like
+    :type src: Union[BinaryIO, bytes, bytearray]
+
+    :param dest: The destination bytes-like
+    :type dest: Union[BinaryIO, bytearray]
+
+    :param src_start: The starting offset to read from the source,
+        defaults to the current position of the stream (BinaryIO) or the start of the buffer (bytes/bytearray)
+    :type src_start: Optional[int], optional
+
+    :param size: The amount of bytes to read from the source,
+        if not specified, will read all bytes possible
+    :type size: Optional[int], optional
+
+    :param dst_start: The starting offset to write to the destination,
+        defaults to the current position of the stream (BinaryIO) or the start of the buffer (bytearray)
+    :type dst_start: Optional[int], optional
+
+    :param chunk_size: The number of bytes to copy from the source to the destination in a single step;
+        by default this is 16 KiB
+    :type chunk_size: int, optional
+
+    """
     if isinstance(dest, bytearray):
         if src_start is None:
             src_start = 0
